541d86ded4164e9d8cd7c506daefc3e40a72130a
Mitchell Hashimoto <mitchell.hashimoto@gmail.com>
2012-12-03 15:52:57 -0800

    Mac OS X 10.8

---
+++ mac_os_x_10_8.sh
@@ -0,0 +1,53 @@
+#!/usr/bin/env bash
+#
+# This bootstraps Puppet on Mac OS X 10.8
+#
+set -e
+
+#--------------------------------------------------------------------
+# Modifiable variables, please set them via environmental variables.
+#--------------------------------------------------------------------
+FACTER_PACKAGE_URL="http://downloads.puppetlabs.com/mac/facter-1.6.16.dmg"
+PUPPET_PACKAGE_URL="http://puppetlabs.com/downloads/mac/puppet-3.0.1.dmg"
+TMP_DIR="/tmp"
+
+#--------------------------------------------------------------------
+# NO TUNABLES BELOW THIS POINT.
+#--------------------------------------------------------------------
+if [ "$EUID" -ne "0" ]; then
+  echo "This script must be run as root." >&2
+  exit 1
+fi
+
+# This function will download a DMG from a URL, mount it, find
+# the `pkg` in it, install that pkg, and unmount the package.
+function install_dmg() {
+  local name="$1"
+  local url="$2"
+  local dmg_path=$(mktemp -t ${name}-dmg)
+
+  echo "Installing: ${name}"
+
+  # Download the package into the temporary directory
+  echo "-- Downloading DMG..."
+  curl -L -o ${dmg_path} ${url} 2>/dev/null
+
+  # Mount it
+  echo "-- Mounting DMG..."
+  local plist_path=$(mktemp -t puppet-bootstrap)
+  hdiutil attach -plist ${dmg_path} > ${plist_path}
+  mount_point=$(grep -E -o -i '/Volumes/[-.a-z0-9]+' ${plist_path})
+
+  # Install. It will be the only pkg in there, so just find any pkg
+  echo "-- Installing pkg..."
+  pkg_path=$(find ${mount_point} -name '*.pkg' -mindepth 1 -maxdepth 1)
+  installer -pkg ${pkg_path} -target / >/dev/null
+
+  # Unmount
+  echo "-- Unmounting and ejecting DMG..."
+  hdiutil eject ${mount_point} >/dev/null
+}
+
+# Install Puppet and Facter
+install_dmg "Puppet" ${PUPPET_PACKAGE_URL}
+install_dmg "Facter" ${FACTER_PACKAGE_URL}
