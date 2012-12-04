72caeb22a341099f660ecb78a68270d4b87beb77
Joshua Gross <joshua@surfeasy.com>
2012-12-04 18:07:02 -0500

    Centos scripts for 5 and 6 that install puppet and facter. Puppet is installed using the repos from puppet labs.

---
+++ centos_5_x.sh
@@ -0,0 +1,24 @@
+#!/usr/bin/env bash
+# This bootsraps Puppet on Centos 5.x
+# It has been tested on centos 5.6 64bit
+
+set -e
+
+REPO_URL="http://yum.puppetlabs.com/el/5/products/i386/puppetlabs-release-5-6.noarch.rpm"
+
+if [ "$EUID" -ne "0" ]; then
+  echo "This script must be run as root." >&2
+  exit 1
+fi
+
+# Install puppet labs repo
+echo "Configuring PuppetLabs repo..."
+repo_path=$(mktemp)
+wget --output-document=${repo_path} ${REPO_URL} 2>/dev/null
+rpm -i ${repo_path} >/dev/null
+
+# Install Puppet...
+echo "Installing puppet"
+yum install -y puppet > /dev/null
+
+echo "Puppet installed!"
