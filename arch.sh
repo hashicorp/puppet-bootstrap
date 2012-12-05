b42f6b3ef48d0c61d0e00c7c3f1ec8f4cec1b471
Mitchell Hashimoto <mitchell.hashimoto@gmail.com>
2012-12-05 14:18:02 -0800

    arch: Run as root

--- arch.sh
+++ arch.sh
@@ -4,6 +4,12 @@
 #
 set -e
 
+# Verify we're running as root
+if [ "$EUID" -ne "0" ]; then
+  echo "This script must be run as root." >&2
+  exit 1
+fi
+
 # Update the pacman repositories
 pacman -Sy
 
