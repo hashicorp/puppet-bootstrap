78488365884b537a1b20c170d52ba9a27a77138b
Mitchell Hashimoto <mitchell.hashimoto@gmail.com>
2013-09-09 14:12:25 -0700

    ubuntu: only output apt-get stderr

--- ubuntu.sh
+++ ubuntu.sh
@@ -40,6 +40,6 @@ echo "Puppet installed!"
 
 # Install RubyGems for the provider
 echo "Installing RubyGems..."
-apt-get install -y rubygems
+apt-get install -y rubygems >/dev/null
 gem install rubygems-update --no-ri --no-rdoc >/dev/null 2>&1
 /var/lib/gems/1.8/bin/update_rubygems >/dev/null 2>&1
