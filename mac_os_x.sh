54c1162856a40e421d397ba2c4a8fbf9287bc31d
Phil Frost <pfrost@macprofessionals.com>
2013-11-22 16:16:47 -0500

    Quote bash variable expansions

--- mac_os_x.sh
+++ mac_os_x.sh
@@ -27,33 +27,33 @@ fi
 function install_dmg() {
   local name="$1"
   local url="$2"
-  local dmg_path=$(mktemp -t ${name}-dmg)
+  local dmg_path=$(mktemp -t "${name}-dmg")
 
   echo "Installing: ${name}"
 
   # Download the package into the temporary directory
   echo "-- Downloading DMG..."
-  curl -L -o ${dmg_path} ${url} 2>/dev/null
+  curl -L -o "${dmg_path}" "${url}" 2>/dev/null
 
   # Mount it
   echo "-- Mounting DMG..."
   local plist_path=$(mktemp -t puppet-bootstrap)
-  hdiutil attach -plist ${dmg_path} > ${plist_path}
-  mount_point=$(grep -E -o '/Volumes/[-.a-zA-Z0-9]+' ${plist_path})
+  hdiutil attach -plist "${dmg_path}" > "${plist_path}"
+  mount_point=$(grep -E -o '/Volumes/[-.a-zA-Z0-9]+' "${plist_path}")
 
   # Install. It will be the only pkg in there, so just find any pkg
   echo "-- Installing pkg..."
-  pkg_path=$(find ${mount_point} -name '*.pkg' -mindepth 1 -maxdepth 1)
-  installer -pkg ${pkg_path} -target / >/dev/null
+  pkg_path=$(find "${mount_point}" -name '*.pkg' -mindepth 1 -maxdepth 1)
+  installer -pkg "${pkg_path}" -target / >/dev/null
 
   # Unmount
   echo "-- Unmounting and ejecting DMG..."
-  hdiutil eject ${mount_point} >/dev/null
+  hdiutil eject "${mount_point}" >/dev/null
 }
 
 # Install Puppet and Facter
-install_dmg "Puppet" ${PUPPET_PACKAGE_URL}
-install_dmg "Facter" ${FACTER_PACKAGE_URL}
+install_dmg "Puppet" "${PUPPET_PACKAGE_URL}"
+install_dmg "Facter" "${FACTER_PACKAGE_URL}"
 
 # Hide all users from the loginwindow with uid below 500, which will include the puppet user
 defaults write /Library/Preferences/com.apple.loginwindow Hide500Users -bool YES
