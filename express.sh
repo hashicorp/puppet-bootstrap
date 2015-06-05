8b94b98ad379a2a7a0aaf79bc79ba0c3aa87b77a
Callahan <callahm3@admins-MacBook-Pro-2.local>
2015-06-05 15:45:44 -0400

    fixed line deletion

--- express.sh
+++ express.sh
@@ -7,6 +7,7 @@ set -e
 
 BOOTSTRAP_TAR_URL=${BOOTSTRAP_TAR_URL:-"https://github.com/edestecd/puppet-bootstrap/archive/master.tar.gz"}
 PLATFORM=${PLATFORM:-$1}
+
 # Attempt to Detect PLATFORM if not set
 if [ -z "${PLATFORM}" ]; then
   case "$(uname -s)" in
