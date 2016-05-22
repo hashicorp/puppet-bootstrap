0cc77dd9b9eec4b884baef1b1370d92a21667395
Paweł Tomulik <ptomulik@meil.pw.edu.pl>
2016-05-22 15:10:35 +0200

    simplify freebsd.sh

--- freebsd.sh
+++ freebsd.sh
@@ -3,18 +3,9 @@
 set -e
 
 # This bootstraps Puppet on FreeBSD
-
-if [ `sysctl -n kern.osreldate` -ge '901000' ] && pkg -N 2> /dev/null ; then
-  have_pkg=true;
-else
-  have_pkg=false;
-fi
-
 echo "Installing Puppet & dependencies..."
-if $have_pkg; then
-	export ASSUME_ALWAYS_YES=yes
-	pkg install "sysutils/puppet4"
-	unset ASSUME_ALWAYS_YES
+if [ `sysctl -n kern.osreldate` -ge '901000' ] && pkg -N 2> /dev/null ; then
+	pkg install -y "sysutils/puppet4"
 else
 	pkg_add -r puppet
 fi
