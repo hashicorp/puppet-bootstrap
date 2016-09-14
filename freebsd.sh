dff4cde1e0c439a9b5cbc951b4985fbc55417a95
Paweł Tomulik <ptomulik@meil.pw.edu.pl>
2016-09-14 23:02:32 +0200

    do not try to run "pkg -N" on freebsd 9.1

--- freebsd.sh
+++ freebsd.sh
@@ -4,7 +4,11 @@ set -e
 
 # This bootstraps Puppet on FreeBSD
 echo "Installing Puppet & dependencies..."
-if [ `sysctl -n kern.osreldate` -ge '901000' ] && pkg -N 2> /dev/null ; then
+# See manpage of pkg(8)
+# https://www.freebsd.org/cgi/man.cgi?query=pkg&apropos=0&sektion=8
+if TMPDIR=/dev/null ASSUME_ALWAYS_YES=1 \
+    PACKAGESITE=file:///nonexistent \
+    pkg info -x 'pkg(-devel)?$' >/dev/null 2>&1; then
 	pkg install -y "sysutils/puppet4"
 else
 	pkg_add -r puppet
